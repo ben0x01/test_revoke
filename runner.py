29f634657eb989d50cff5ebc1937aade3d304d7a
ben0x01 <122745733+ben0x01@users.noreply.github.com>
2024-08-19 04:10:14 +0500

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,5 +1,6 @@
 from src.contract_revoke import ApproverManager
 from src.single_revoke import TokenAuthorizedListFetcher
+from src.mass_revoke import MassTokenAuthorizedListFetcher
 
 async def run_contract_revoke(private_key):
     manager = ApproverManager(private_key)
@@ -11,3 +12,9 @@ async def run_single_rabby_revoke():
     chain = input("Write chain: ")
     manager_2 = TokenAuthorizedListFetcher(address, chain)
     await manager_2.run()
+
+
+async def run_mass_rabby_revoke():
+    address = input("Write wallet address: ")
+    manager_3 = MassTokenAuthorizedListFetcher(address)
+    await manager_3.run()
