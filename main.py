41a86962265fa4664e5dec678244f450b460ef6a
ben0x01 <122745733+ben0x01@users.noreply.github.com>
2024-08-19 04:10:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import asyncio
-from runner import run_contract_revoke, run_single_rabby_revoke
+from runner import run_contract_revoke, run_single_rabby_revoke, run_mass_rabby_revoke
 
 
 def main_menu():
     print("Main Menu")
     print("1. Run revoke with contract")
     print("2. Run single revoke from rabby")
-    print("3. Exit")
+    print("3. Run mass revoke from rabby")
+    print("4. Exit")
+
 
     choice = input("Choose an option: ").strip()
 
@@ -16,6 +18,8 @@ def main_menu():
     elif choice == '2':
         asyncio.run(run_single_rabby_revoke())
     elif choice == '3':
+        asyncio.run(run_mass_rabby_revoke())
+    elif choice == '4':
         print("Exiting...")
         return
     else:
@@ -25,4 +29,3 @@ def main_menu():
 
 if __name__ == "__main__":
     main_menu()
-
