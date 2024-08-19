551c89afaf9f25f275b758beb7efbbb4d758f2fc
ben0x01 <122745733+ben0x01@users.noreply.github.com>
2024-08-19 16:07:48 +0500

    Update runner.py

--- runner.py
+++ runner.py
@@ -2,19 +2,39 @@ from src.contract_revoke import ApproverManager
 from src.single_revoke import TokenAuthorizedListFetcher
 from src.mass_revoke import MassTokenAuthorizedListFetcher
 
-async def run_contract_revoke(private_key):
-    manager = ApproverManager(private_key)
-    await manager.main()
+
+def load_private_keys(file_path='keys.txt'):
+    with open(file_path, 'r') as file:
+        keys = [line.strip() for line in file.readlines() if line.strip()]
+    return keys
+
+
+async def run_contract_revoke():
+    private_keys = load_private_keys()
+    if not private_keys:
+        print("No private keys found in keys.txt")
+        return
+    for private_key in private_keys:
+        manager = ApproverManager(private_key)
+        await manager.main()
 
 
 async def run_single_rabby_revoke():
-    address = input("Write wallet address: ")
-    chain = input("Write chain: ")
-    manager_2 = TokenAuthorizedListFetcher(address, chain)
-    await manager_2.run()
+    private_keys = load_private_keys()
+    if not private_keys:
+        print("No private keys found in keys.txt")
+        return
+    for private_key in private_keys:
+        chain = input("Write chain: ")
+        manager_2 = TokenAuthorizedListFetcher(chain, private_key)
+        await manager_2.run()
 
 
 async def run_mass_rabby_revoke():
-    address = input("Write wallet address: ")
-    manager_3 = MassTokenAuthorizedListFetcher(address)
-    await manager_3.run()
+    private_keys = load_private_keys()
+    if not private_keys:
+        print("No private keys found in keys.txt")
+        return
+    for private_key in private_keys:
+        manager_3 = MassTokenAuthorizedListFetcher(private_key)
+        await manager_3.run()
